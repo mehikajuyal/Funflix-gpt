42d9c258683816a925ac522573bc313c03f143cd
Mehika Juyal <mehikajuyal@gmail.com>
2025-06-06 16:13:16 +0530

    part 2 commit added parcel, git ignore

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 // const parent = React.createElement('h1', {},"Hello World" );
-const parent = React.createElement("div", { class: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", { id: "heading1" }, "This is heading 1"),
-    React.createElement("h1", { id: "heading2" }, "This is heading 2"),
+const parent = React.createElement("div", { id: "parent", key: "parent" }, [
+  React.createElement("div", { id: "child1", key: "child1" }, [
+    React.createElement(
+      "h1",
+      { id: "heading1", key: "heading1" },
+      "This is heading 1"
+    ),
+    React.createElement(
+      "h2",
+      { id: "heading2", key: "heading2" },
+      "This is heading 2"
+    ),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { id: "heading3" }, "This is heading 1"),
-    React.createElement("h1", { id: "heading4" }, "This is heading 2"),
+  React.createElement("div", { id: "child2", key: "child2" }, [
+    React.createElement(
+      "h3",
+      { id: "heading3", key: "heading3" },
+      "This is heading 3"
+    ),
+    React.createElement(
+      "h4",
+      { id: "heading4", key: "heading4" },
+      "This is heading 9"
+    ),
   ]),
 ]);
 
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Learn-react
+
 Learning React and coding
+
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR -mHot Module Replacement
+- File watching Algorithms - written in C++
+- Caching - Faster builds - .parcel-cache
+- Image Optimisation
+- In production builds minification
+- Bundling
+- Compress
+- Consistent hashing
+- Code splitting
+- Differential bundling
+- Support older browsers
+- Diagnostic of your app
+- Error handling
+- HTTPS
+- Tree shaking - remove unused code
+- Different Dev and Prod bundles
